8c8424b9ff0ee56e5e35f65add52172bc6acec9a
JokiestJoke <mdoghram0@gmail.com>
2021-03-10 09:38:16 -0500

    some further work on directional movement code and animations

--- SecondScene.js
+++ SecondScene.js
@@ -42,7 +42,7 @@ class SecondScene extends Phaser.Scene {
         }); 
 
         this.anims.create({
-            key: 'playerMove',
+            key: 'playerMoveLRD',
             frames: this.anims.generateFrameNumbers(
                 'admiralSpriteOne',
                 {
@@ -52,6 +52,19 @@ class SecondScene extends Phaser.Scene {
                 frameRate: 6,
                 repeat: -1       
         });
+
+        this.anims.create({
+            key: 'playerMoveUp',
+            frames: this.anims.generateFrameNumbers(
+                'admiralSpriteOne',
+                {
+                    start: 28,
+                    end: 36
+
+                }),
+                frameRate: 6,
+                repeat: -1
+        })
 
         //initialize button inputs
         gameState.cursors = this.input.keyboard.createCursorKeys();
@@ -63,19 +76,53 @@ class SecondScene extends Phaser.Scene {
     update() {
         // creating a function for 
         const playerCursorHandler = () => {
-            if (gameState.cursors.up.isDown) {
-                console.log('up is down');
-            } else if (gameState.cursors.right.isDown){
-                console.log('right is down');
-                gameState.PlayerSprite.anims.play('playerMove', true).flipX=false;
+            // Major thins i want to do to this function. 1) see if i can define it as a method and simply call the function in the update.
+            // 2) a last direction array or something..... this is to handle which direction the sprite rests at AKA faces after moving
+            if (gameState.cursors.right.isDown && gameState.cursors.up.isDown) {
+                //handling a diagnol upward right movement.
+                console.log('Up and Right is down');
+                gameState.PlayerSprite.anims.play('playerMoveLRD', true).flipX=false;
+                gameState.PlayerSprite.x += 2;
+                gameState.PlayerSprite.y -= 2
+            } else if (gameState.cursors.left.isDown && gameState.cursors.up.isDown) {
+                 //handling a diagnol upward left movement.
+                 console.log('Up and left is down');
+                 gameState.PlayerSprite.anims.play('playerMoveLRD', true).flipX=true;
+                 gameState.PlayerSprite.x -= 2;
+                 gameState.PlayerSprite.y -= 2
+            } else if (gameState.cursors.right.isDown && gameState.cursors.down.isDown) {
+                //handling a diagnol downward right movement.
+                console.log('Down and Right is down');
+                gameState.PlayerSprite.anims.play('playerMoveLRD', true).flipX=false;
+                gameState.PlayerSprite.x += 2;
+                gameState.PlayerSprite.y += 2
+            } else if (gameState.cursors.left.isDown && gameState.cursors.down.isDown) {
+                // handling a diagnol downward left movement
+                console.log('Down and Left is down');
+                gameState.PlayerSprite.anims.play('playerMoveLRD', true).flipX=true;
+                gameState.PlayerSprite.x -= 2;
+                gameState.PlayerSprite.y += 2;
+            } else if (gameState.cursors.right.isDown) {
+                //handling rightward movement of player
+                // console.log('right is down');
+                gameState.PlayerSprite.anims.play('playerMoveLRD', true).flipX=false;
+                gameState.PlayerSprite.x += 2;
             } else if (gameState.cursors.left.isDown) {
-                console.log('left is down');
-                gameState.PlayerSprite.anims.play('playerMove', true).flipX=true;
+                // handling leftward movement of player
+                // console.log('left is down');
+                gameState.PlayerSprite.anims.play('playerMoveLRD', true).flipX=true;
+                gameState.PlayerSprite.x -= 2;
             } else if (gameState.cursors.down.isDown) {
-                console.log('down is down')
-            }  else if (gameState.cursors.space.isDown) { 
+                //handling 'southward' movement
+                // console.log('down is down')
+                gameState.PlayerSprite.y += 2;
+            } else if (gameState.cursors.space.isDown) {                           
                 gameState.PlayerSprite.anims.play('playerTalk', true);
-
+            } else if (gameState.cursors.up.isDown) {
+                //handling purely upward movement
+                // console.log('up is down');
+                gameState.PlayerSprite.y -= 2;
+                gameState.PlayerSprite.anims.play('playerMoveUp', true);
             } else {
                 gameState.PlayerSprite.anims.play('idlePlayer', true);
             }
